945ac0d23d94aa63eb1318a887e98dce480cd724
Cristian <cristianjose@gmail.com>
2020-10-08 16:01:12 -0600

    commit inicial

---
+++ ProjectoAnalisis/src/main/java/com/Analisis2/ProjectoAnalisisApplication.java
@@ -0,0 +1,13 @@
+package com.Analisis2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoAnalisisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectoAnalisisApplication.class, args);
+	}
+
+}
---
+++ ProjectoAnalisis/src/test/java/com/Analisis2/ProjectoAnalisisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Analisis2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectoAnalisisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
